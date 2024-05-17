fc383a90b53d99ad5a6c8907ceaaabb8c9826b09
Zinramen <fhgh6290@gmail.com>
2024-05-18 02:17:54 +0900

    Boss는 피격 시 회전 X

--- Assets/Hyun/Scripts/Entity.cs
+++ Assets/Hyun/Scripts/Entity.cs
@@ -320,11 +320,13 @@ public class Entity : MonoBehaviour
                 }
 
                 // 맞는 방향으로 회전
-                if (thrustValue < 0)
-                    transform.localEulerAngles = new Vector3(0, 0, 0);
-                else
-                    transform.localEulerAngles = new Vector3(0, 180, 0);
-
+                if (!GameObject.CompareTag("Boss"))
+                {
+                    if (thrustValue < 0)
+                        transform.localEulerAngles = new Vector3(0, 0, 0);
+                    else
+                        transform.localEulerAngles = new Vector3(0, 180, 0);
+                }
                 if (HitEffect && damageValue > 0)
                 {
                     PlayHitEffect(damageValue);
