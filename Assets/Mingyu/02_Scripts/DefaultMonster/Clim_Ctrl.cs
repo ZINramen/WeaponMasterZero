35453908d4fb28fbe6d960988d21294fe55baf66
choimingue-game <20191270@game.hoseo.edu>
2024-05-31 06:59:34 +0900

    24.05.31 Slime Finish
    슬라임 전부 제작 완료

--- Assets/Mingyu/02_Scripts/DefaultMonster/Clim_Ctrl.cs
+++ Assets/Mingyu/02_Scripts/DefaultMonster/Clim_Ctrl.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class Clim_Ctrl : HitColider
 {
     protected override void EachObj_HitSetting(Collider2D other)
-    {   
-        if (other.gameObject.tag == "Player")
-            return;
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.layer == 10)
+        {
+            if (DestroyEffect)
+            {
+                GameObject dummyEffect = Instantiate(DestroyEffect, transform.position, Quaternion.identity);
+                this.gameObject.GetComponent<Rigidbody2D>().simulated = false;
+                Destroy(this.gameObject, dummyEffect.gameObject.GetComponent<ParticleSystem>().duration);
+            }
+        }
     }
 }
