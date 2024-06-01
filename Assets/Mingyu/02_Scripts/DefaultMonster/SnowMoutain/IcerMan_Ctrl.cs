c8000a1ce75aabac53d2c8aa2d3518bb7458651f
choimingue-game <20191270@game.hoseo.edu>
2024-06-01 15:12:25 +0900

    24.06.01 Default Monster
    방패병 빼고 다함

---
+++ Assets/Mingyu/02_Scripts/DefaultMonster/SnowMoutain/IcerMan_Ctrl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcerMan_Ctrl : Default_Monster
+{
+        [SerializeField] private float m_dAtt_dist;
+        [SerializeField] private float m_traceDist;
+
+        private Entity icerEntity;
+        
+        void Start()
+        {
+            base.Start();
+            monsterState = new Default_MonsterState();
+            Init_StateValueData(ref monsterState);
+    
+            stopDelayTime = 1.5f;
+    
+            AttHitCol = this.gameObject.transform.GetChild(0).gameObject;
+            AttHitCol.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+            // 야매
+            icerEntity = this.gameObject.GetComponent<Entity>();
+            icerEntity.playerFinalBoss = player.gameObject.GetComponent<Entity>();
+            icerEntity.activeDesireWeapon = true;
+            
+            icerEntity.desireWeaponFinalBoss = (int)(HitColider.AttackType.Player_GunAtt);
+        }
+    
+        protected override void Init_StateValueData(ref Default_MonsterState state)
+        {
+            state.defaultAtt_dist = m_dAtt_dist;
+            state.traceDistance = m_traceDist;
+        }
+}
