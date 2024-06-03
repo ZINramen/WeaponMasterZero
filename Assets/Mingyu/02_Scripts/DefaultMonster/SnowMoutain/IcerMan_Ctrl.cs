9be57fafe9a6a0e22427f6aa312f9f27c5bed196
choimingue-game <20191270@game.hoseo.edu>
2024-06-04 05:53:23 +0900

    24.06.04 Test

--- Assets/Mingyu/02_Scripts/DefaultMonster/SnowMoutain/IcerMan_Ctrl.cs
+++ Assets/Mingyu/02_Scripts/DefaultMonster/SnowMoutain/IcerMan_Ctrl.cs
@@ -7,8 +7,11 @@ public class IcerMan_Ctrl : Default_Monster
 {
         [SerializeField] private float m_dAtt_dist;
         [SerializeField] private float m_traceDist;
+        [SerializeField] private GameObject Parringbullet;
+        [SerializeField] private float parringSpeed;
 
         private Entity icerEntity;
+        private Transform ParringPos;
         
         void Start()
         {
@@ -21,12 +24,14 @@ public class IcerMan_Ctrl : Default_Monster
             AttHitCol = this.gameObject.transform.GetChild(0).gameObject;
             AttHitCol.gameObject.GetComponent<BoxCollider2D>().enabled = false;
 
+            ParringPos = this.gameObject.transform.GetChild(0).transform;
+
             // 야매
             icerEntity = this.gameObject.GetComponent<Entity>();
             icerEntity.playerFinalBoss = player.gameObject.GetComponent<Entity>();
             icerEntity.activeDesireWeapon = true;
             
-            icerEntity.desireWeaponFinalBoss = (int)(HitColider.AttackType.Player_GunAtt);
+            icerEntity.desireWeaponFinalBoss = (int)(HitColider.AttackType.Player_SwordAtt);
         }
     
         protected override void Init_StateValueData(ref Default_MonsterState state)
@@ -39,10 +44,18 @@ public class IcerMan_Ctrl : Default_Monster
         {
             if (other.gameObject.GetComponent<HitColider>())
             {
-                if (other.gameObject.GetComponent<HitColider>().attType == HitColider.AttackType.Player_SwordAtt)
+                if (other.gameObject.GetComponent<HitColider>().attType == HitColider.AttackType.Player_GunAtt)
                 {
-                    other.transform.parent.GetComponent<Movement>().SetMovementForceX(-50);
-                    other.transform.parent.GetComponent<Animator>().SetTrigger("Hit");
+                    Debug.Log("맞음" + other.gameObject.name);
+                    
+                     GameObject dummyBullet = GameObject.Instantiate(Parringbullet, ParringPos.position, Quaternion.identity);
+                     dummyBullet.gameObject.GetComponent<HitColider>().owner = icerEntity;
+
+                     Vector3 AttackDir = (this.transform.position - other.transform.position).normalized;
+                     
+                    dummyBullet.gameObject.GetComponent<Rigidbody2D>()
+                        .AddForce(parringSpeed * -AttackDir, ForceMode2D.Impulse);
+
                 }
             }
         }
