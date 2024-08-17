af59c52d79701284a010506bf51b4559b2ba8656
Zinramen <fhgh6290@gmail.com>
2024-08-18 01:54:37 +0900

    보스 즉시 이동 버튼 + 단축키 추가 + 버그 수정 + 스킵 기능
    <보스 즉시 이동>
    메뉴에 추가됨
    
    <단축키>
    O : 전부 리셋
    P : 모든 챕터 개방
    ESC : 메뉴로 이동
    
    <버그 수정>
    이제 멈췄을 때 총 사용 불가
    
    <스킵 기능>
    일부 긴 컷씬에 스킵 추가

--- Assets/Hyun/Scripts/BaseGameSettings.cs
+++ Assets/Hyun/Scripts/BaseGameSettings.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BaseGameSettings : MonoBehaviour
 {
@@ -13,5 +15,19 @@ public class BaseGameSettings : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("Title_Prologue", LoadSceneMode.Single);
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PlayerPrefs.SetInt("cur_chapter", 4);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            PlayerPrefs.DeleteAll();
+        }
     }
 }
