da679a7a5659c0934fb666b5295a73f9171b8f1a
Zinramen <fhgh6290@gmail.com>
2024-09-14 14:48:05 +0900

    주석 추가

--- Assets/Hyun/Scripts/ShootingControl.cs
+++ Assets/Hyun/Scripts/ShootingControl.cs
@@ -43,21 +43,27 @@ public class ShootingControl : MonoBehaviour
     {
         if(owner.movement.PlayerType)
         if(!owner.isDie)
-        if (WhenTargeting)
+        if (WhenTargeting) // 플레이어가 총을 무기로 사용하고 있으며 사격 가능한 상태
         {
-            Cursor.SetCursor(targetingImage, new Vector2(targetingImage.width/3f, targetingImage.height / 3f), CursorMode.Auto);
-            AtargetingPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(20, -20, 0));
+            Cursor.SetCursor(targetingImage, new Vector2(targetingImage.width/3f, targetingImage.height / 3f), CursorMode.Auto); // 커서를 사격 조준경으로 바꿈.
+            AtargetingPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(20, -20, 0)); // 마우스 위치를 변수에 저장함.
 
+            // gun은 총 오브젝트를 의미하며 아래 변수들은 마우스의 방향에 따라 총의 회전값이 변하도록 하였다.
             var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
             var angle = MathF.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
             
             gun.transform.rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+
+            // 총을 사용 가능한 상태에선 gunAnimator라는 애니메이터를 이용해 총의 발사 애니메이션을 보여준다.
+            // 아래는 gunAnimator를 가진 게임 오브젝트의 회전 값을 바꾸어 총의 위치가 제대로 위치하도록 하는 코드.
             gunAnimator.transform.localEulerAngles = new Vector3(gunAnimator.transform.localEulerAngles.x, owner.transform.localEulerAngles.y, gunAnimator.transform.localEulerAngles.z);
 
-            if (bulletCount < bulletMaxCount)
+            if (bulletCount < bulletMaxCount) // 총의 발사 횟수 제한이 있고, 해당 횟수를 넘어서면 일정 시간 동안 재장전이 이뤄진다.
             {
-                if (Input.GetKeyUp(KeyCode.Mouse0))
+                if (Input.GetKeyUp(KeyCode.Mouse0)) 
                 {
+                    // 아래는 총을 발사하는 과정을 나타낸 코드이다.
+
                     bulletCount++;
                     if (GunGauge.fillAmount > 0.8f)
                     {
@@ -70,9 +76,11 @@ public class ShootingControl : MonoBehaviour
                     var obj = Instantiate(bullet, fireTr.position, Quaternion.identity);
                     obj.GetComponent<HitColider>().owner = owner;
                     Destroyer d = obj.GetComponent<Destroyer>();
+
                     obj.transform.LookAt(AtargetingPos);
                     d.moveSpeed = bulletSpeed;
                     d.haveTarget = true;
+
                     if (gunAnimator != null)
                     {
                         gunAnimator.SetTrigger("Shoot");
@@ -80,7 +88,7 @@ public class ShootingControl : MonoBehaviour
                     owner.aManager.ResetAttackTriggerEvent();
                 }
                 if (bulletCount == bulletMaxCount)
-                    StartCoroutine(Reload());
+                    StartCoroutine(Reload()); // 재장전을 담당하는 코루틴 호출
             }
         }
         else
@@ -107,10 +115,12 @@ public class ShootingControl : MonoBehaviour
 
     public void SimpleShoot(float speed)
     {
+        // 총알이 마우스 방향을 향하도록 하는 코드
         Vector2 len = this.gameObject.GetComponent<Movement>().mousePos - this.gameObject.transform.position;
         float z = Mathf.Atan2(len.y, len.x) * Mathf.Rad2Deg;
-        
         var obj = Instantiate(bullet, GunfireTr.position, Quaternion.Euler(0f, 0f, z));
+
+        // 총알의 속도를 조절하고, 플레이어는 해당 총알에 맞지 않도록 설정함.
         obj.GetComponent<HitColider>().owner = owner;
         Destroyer d = obj.GetComponent<Destroyer>();
         d.moveSpeed = speed;
@@ -130,10 +140,12 @@ public class ShootingControl : MonoBehaviour
         float tempPosX;
         if (owner.transform.rotation.y == 0) tempPosX = 1.5f;
         else tempPosX = -1.5f;
-        
+        // 바위를 생성해 앞쪽으로 날린다.
         var stone = Instantiate(StonePrefab, owner.transform.position + new Vector3(tempPosX, 0.3f),
             Quaternion.identity);
         stone.GetComponent<Rigidbody2D>().AddForce(owner.transform.right * 10000);
+
+        // 바위는 플레이어가 맞지 않도록 만든 오브젝트라 별도 처리가 필요 없다.
     }
     
     public void ShockWave()
@@ -141,9 +153,12 @@ public class ShootingControl : MonoBehaviour
         float tempPosX;
         if (owner.transform.rotation.y == 0) tempPosX = 0.5f;
         else tempPosX = -0.5f;
-        
+
+        // 참격을 생성해 앞쪽으로 날린다.
         var shockWave = Instantiate(ShockWavePrefab, owner.transform.position + new Vector3(tempPosX, -0.5f),
             Quaternion.identity);
+
+        // 참격의 경우 플레이어가 맞지 않게 설정함.
         shockWave.GetComponent<HitColider>().owner = owner;
         shockWave.GetComponent<Rigidbody2D>().AddForce(owner.transform.right * 10000);
     }
--- Assets/Hyun/Scripts/SkillManager.cs
+++ Assets/Hyun/Scripts/SkillManager.cs
@@ -19,35 +19,35 @@ public class SkillManager : MonoBehaviour
     {
         owner = GetComponent<Entity>();
 
-        if (hudControl)
+        if (hudControl) // 플레이어가 보는 UI에서 currentWeapon 값에 해당하는 무기가 무엇인지 보여줌.
             hudControl.ChangeCurrentWeapon(currentWeapon);
 
-        ChangeWeaponSkill(false, currentWeapon);
-        CheckMPUI();
+        ChangeWeaponSkill(false, currentWeapon); // currentWeapon 값에 해당하는 무기로 현재 무기 바꿈,
+        CheckMPUI(); // Gauge 값을 체크하여 Gauge가 얼마나 모였는지 표시함. (Gauge는 보라색 마름모로 표시함. Gauge가 3이면 보라색 마름모가 3개이다.)
     }
 
     private void Update()
     {
         if (owner)
-            owner.aManager.ani.SetInteger("Weapon", currentWeapon);
+            owner.aManager.ani.SetInteger("Weapon", currentWeapon); // 애니메이터의 파라미터 값을 바꿔 현재 무기에 맞는 애니메이션이 재생되도록 함.
         
         if (hudControl)
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
-                ChangeWeaponSkill(true);
+                ChangeWeaponSkill(true); // 왼쪽무기와 현재무기교체 (현재 무기는 왼쪽으로 이동)
             }
 
             if (Input.GetKeyDown(KeyCode.E))
             {
-                ChangeWeaponSkill(false);
+                ChangeWeaponSkill(false); // 오른쪽무기와 현재무기교체 (현재 무기는 오른쪽으로 이동)
             }
             if (owner.GetHp() > 0 && Input.GetKeyDown(KeyCode.W)) // 체력 회복
             {
-                int curGauge = owner.aManager.ani.GetInteger("Gauge");
-                if (curGauge > 0)
+                int curGauge = owner.aManager.ani.GetInteger("Gauge"); // 애니메이터에서 Gauge 값을 가져옴. Gauge는 스킬, 필살기 사용에 필요한 값이다.
+                if (curGauge > 0) // Gauge 값이 0보다 크면 게이지 값 1을 소모하여 체력 회복이 가능하다.
                 {
-                    MedalControlSystem.hpBeforeChange = (int)owner.GetHp();
+                    MedalControlSystem.hpBeforeChange = (int)owner.GetHp(); 
                     Instantiate(healEffect, owner.transform.position, Quaternion.identity).transform.parent = owner.transform;
                     owner.SetHp(owner.GetHp() + owner.maxHP * 0.3f); // 체력 회복 퍼센테이지 0.3
                     isHealthEvent = true;
@@ -55,42 +55,42 @@ public class SkillManager : MonoBehaviour
                 }
             }
         }
-        if (owner.GetMp() == owner.maxMp) 
+        if (owner.GetMp() == owner.maxMp) // 현재 MP값이 최대치일 때 처리
         {
             int curGauge = owner.aManager.ani.GetInteger("Gauge");
-            if (curGauge < 3)
+            if (curGauge < 3) // 값이 3이 아닐 경우 게이지 값이 증가한다.
             {
                 owner.aManager.ani.SetInteger("Gauge", curGauge + 1);
                 CheckMPUI();
             }
-            owner.SetMp(0);
+            owner.SetMp(0); // MP값이 0으로 초기화됨.
         }
     }
 
 
-    public void ChangeWeaponSkill(bool isLeftWeapon, int newWeapon = -1) 
+    public void ChangeWeaponSkill(bool isLeftWeapon, int newWeapon = -1) // 첫번째 매개변수는 왼쪽 무기와 교체하는지 여부, 두번째 매개변수는 바꿀 무기의 번호이다.
     {
-        var previousWeapon = currentWeapon;
-        if (newWeapon == -1)
+        var previousWeapon = currentWeapon; // 교체 전 무기를 변수로 저장
+        if (newWeapon == -1)  // 무기의 번호를 매개변수로 전달 안 할 경우
         {
-            if (isLeftWeapon)
-                currentWeapon = currentLeftWeapon;
+            if (isLeftWeapon) // 왼쪽으로 교체하는 버튼을 눌렀을 경우
+                currentWeapon = currentLeftWeapon; // 현재 무기를 기본 값으로 설정된 왼쪽무기로 바꿈.
             else
-                currentWeapon = currentRightWeapon;
+                currentWeapon = currentRightWeapon; // 현재 무기를 기본 값으로 설정된 오른쪽무기로 바꿈.
         }
         else
-            currentWeapon = newWeapon;
+            currentWeapon = newWeapon; // 무기 번호에 맞는 무기로 바뀜.
 
-        if(haveWeaponNum < currentWeapon) 
+        if(haveWeaponNum < currentWeapon) // 바꾼 무기의 번호(무기 번호는 얻는 순서와 같음)가 현재 보유한 무기의 수보다 작을 경우
         {
-            currentWeapon = previousWeapon;
+            currentWeapon = previousWeapon; // 이전 무기로 다시 바꿈.
             return;
         }
 
-        hudControl.ChangeCurrentWeapon(currentWeapon);
+        hudControl.ChangeCurrentWeapon(currentWeapon); // 현재 무기를 UI에 반영
 
         bool left = false;
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++) // 다음 왼쪽 무기와 오른쪽 무기를 번호 순서에 따라 정함.
         {
             if (i != (currentWeapon - 1))
             {
@@ -105,7 +105,7 @@ public class SkillManager : MonoBehaviour
         }
     }
 
-    public void CheckMPUI()
+    public void CheckMPUI() 
     {
         int curGauge = owner.aManager.ani.GetInteger("Gauge");
         for (int i = 0; i < hudControl.GaugeIcons.Length; i++)
